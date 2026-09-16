82c5f9ba6a46c86bb666b42ea924708c8f43987c
xiaoweiurl <95414283+xiaoweiurl@users.noreply.github.com>
2026-09-16 14:22:29 +0800

    Continue MyBatis migration: remaining files

--- backend/src/test/java/com/cairui/finreport/FinReportApplicationTests.java
+++ backend/src/test/java/com/cairui/finreport/FinReportApplicationTests.java
@@ -2,10 +2,8 @@ package com.cairui.finreport;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("local")
 class FinReportApplicationTests {
     @Test
     void contextLoads() {
